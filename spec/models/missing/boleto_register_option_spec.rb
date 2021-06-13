e22f10bfcd54d13530e88238c5a05736326b15c6
Mariana_C_Ruiz <mchierandaruiz0@gmail.com>
2021-06-13 07:22:38 -0300

    tested boleto register model

--- spec/models/missing/boleto_register_option_spec.rb
+++ spec/models/missing/boleto_register_option_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe BoletoRegisterOption, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+describe BoletoRegisterOption do
+  let(:company) {Company.create!(corporate_name: 'Codeplay SA', cnpj: '11.222.333/0001-44' , state: 'São Paulo', 
+                                  city: 'Campinas', district: 'Inova', street: 'rua 1', number: '12', 
+                                  address_complement: '', billing_email: 'faturamento@codeplay.com')}
+  let(:boleto) {BoletoRegisterOption.new(company: company, payment_option: pay_1, bank_code: bank,
+                                         agency_number: '2050', account_number: '123.555-8')}
+  let(:user_admin) {User.create!(email: 'admin@codeplay.com', password: '123456', role: 1, boleto: boleto)}
+  let(:user) {User.create!(email: 'user@codeplay.com', password: '123456', role: 0, boleto: boleto)}    
+  let(:pay_1) {PaymentOption.create!(name: 'Boleto', fee: 1.9, max_money_fee: 20, payment_type: 0)}
+  let(:bank) {BankCode.create!(code: '001', bank:'Banco do Brasil S.A.')}
+  let(:bank2) {BankCode.create!(code: '029', bank:'Itau')}
+  
+  it 'attributes cannot be blank' do
+    boleto = BoletoRegisterOption.new
+    boleto.valid?
+
+    expect(boleto.errors[:bank_code]).to include('é obrigatório(a)')
+    expect(boleto.errors[:agency_number]).to include('não pode ficar em branco')
+    expect(boleto.errors[:account_number]).to include('não pode ficar em branco')
+  end
+  it ' must be uniq scope agency and bank' do
+    BoletoRegisterOption.create!(company: company, payment_option: pay_1, bank_code: bank, agency_number: '2050', account_number: '123.555-8')
+    boleto_2 = BoletoRegisterOption.new(company: company, payment_option: pay_1, bank_code: bank, agency_number: '2050', account_number: '123.555-8')
+    boleto_2.valid?
+
+    expect(boleto_2.errors[:account_number]).to include('já está em uso')    
+  end
+  it 'do not need to be uniq scope just agency, different bank' do
+    BoletoRegisterOption.create!(company: company, payment_option: pay_1, bank_code: bank, agency_number: '2050', account_number: '123.555-8')
+    boleto_2 = BoletoRegisterOption.new(company: company, payment_option: pay_1, bank_code: bank2, agency_number: '2050', account_number: '123.555-8')
+    boleto_2.valid?
+
+    expect(boleto_2.errors).to be_empty  
+  end
+end       
--- spec/system/paynow_client/payment/client_admin_edit_boleto_spec.rb
+++ spec/system/paynow_client/payment/client_admin_edit_boleto_spec.rb
@@ -5,13 +5,12 @@ describe 'edit boleto' do
                 city: 'Campinas', district: 'Inova', street: 'rua 1', number: '12', 
                 address_complement: '', billing_email: 'faturamento@codeplay.com')}
   let(:user_admin) {User.create!(email: 'admin@codeplay.com', password: '123456', role: 1, company: company)}
-  let(:user) {User.create!(email: 'user@codeplay.com', password: '123456', role: 0, company: company)}
   let(:pay_1) {PaymentOption.create!(name: 'Boleto', fee: 1.9, max_money_fee: 20, payment_type: 0)}
+  let(:pay_2) {PaymentOption.create!(name: 'Boleto 2', fee: 1.9, max_money_fee: 20, payment_type: 0)}
   let(:bank) {BankCode.create!(code: '001', bank:'Banco do Brasil S.A.')}
 
   it 'successfuly' do
     DomainRecord.create!(email_client_admin: user_admin, domain: 'codeplay.com', company: company)
-    DomainRecord.create!(email: user, domain: 'codeplay.com', company: company)
     BoletoRegisterOption.create!(company: company, payment_option: pay_1, bank_code: bank, agency_number: '2050', account_number: '123.555-8')
     PaymentCompany.create!(company: company, payment_option: pay_1)
 
@@ -31,7 +30,6 @@ describe 'edit boleto' do
   end
   it 'cannot be blank' do
     DomainRecord.create!(email_client_admin: user_admin, domain: 'codeplay.com', company: company)
-    DomainRecord.create!(email: user, domain: 'codeplay.com', company: company)
     BoletoRegisterOption.create!(company: company, payment_option: pay_1, bank_code: bank, agency_number: '2050', account_number: '123.555-8')
     PaymentCompany.create!(company: company, payment_option: pay_1)
 
@@ -47,7 +45,67 @@ describe 'edit boleto' do
     expect(page).to have_content('não pode ficar em branco', count: 2)
   end
   context 'uniquenes' do
-    xit 'account number' do    
+    it 'account number same bank and agency' do    
+      DomainRecord.create!(email_client_admin: user_admin, domain: 'codeplay.com', company: company)
+      BoletoRegisterOption.create!(company: company, payment_option: pay_1, bank_code: bank, agency_number: '2050', account_number: '123.555-8')
+      BoletoRegisterOption.create!(company: company, payment_option: pay_2, bank_code: bank, agency_number: '2150', account_number: '123.555-7')
+      PaymentCompany.create!(company: company, payment_option: pay_1)
+      PaymentCompany.create!(company: company, payment_option: pay_2)
+
+      login_as user_admin, scope: :user
+      visit client_admin_company_path(company[:token])
+      click_on 'Opções de pagamento'
+      click_on 'Atualizar Boleto'
+      select "#{bank.code} - #{bank.bank}", from: 'Código do banco'
+      fill_in 'Agência', with: '2150'
+      fill_in 'Número da conta', with: '123.555-7'
+      click_on 'Atualizar'
+    
+      expect(page).to have_content('já está em uso')
+    end
+    it 'account number same bank different agency' do    
+      DomainRecord.create!(email_client_admin: user_admin, domain: 'codeplay.com', company: company)
+      BoletoRegisterOption.create!(company: company, payment_option: pay_1, bank_code: bank, agency_number: '2050', account_number: '123.555-8')
+      BoletoRegisterOption.create!(company: company, payment_option: pay_2, bank_code: bank, agency_number: '2150', account_number: '123.555-7')
+      PaymentCompany.create!(company: company, payment_option: pay_1)
+      PaymentCompany.create!(company: company, payment_option: pay_2)
+
+      login_as user_admin, scope: :user
+      visit client_admin_company_path(company[:token])
+      click_on 'Opções de pagamento'
+      click_on 'Atualizar Boleto'
+      select "#{bank.code} - #{bank.bank}", from: 'Código do banco'
+      fill_in 'Agência', with: '2350'
+      fill_in 'Número da conta', with: '123.555-7'
+      click_on 'Atualizar'
+    
+      expect(page).to have_content('Opção atualizada com sucesso')
+      expect(page).to have_content('001')
+      expect(page).to have_content('2350')
+      expect(page).to have_content('123.555-7')
+    end
+    it 'account number same agency different bank' do    
+      DomainRecord.create!(email_client_admin: user_admin, domain: 'codeplay.com', company: company)
+      bank2 = BankCode.create!(code: '029', bank:'Itau')
+      BoletoRegisterOption.create!(company: company, payment_option: pay_1, bank_code: bank, agency_number: '2050', account_number: '123.555-8')
+      BoletoRegisterOption.create!(company: company, payment_option: pay_2, bank_code: bank2, agency_number: '2150', account_number: '123.555-7')
+      PaymentCompany.create!(company: company, payment_option: pay_1)
+      PaymentCompany.create!(company: company, payment_option: pay_2)
+      
+
+      login_as user_admin, scope: :user
+      visit client_admin_company_path(company[:token])
+      click_on 'Opções de pagamento'
+      click_on 'Atualizar Boleto'
+      select "#{bank.code} - #{bank.bank}", from: 'Código do banco'
+      fill_in 'Agência', with: '2150'
+      fill_in 'Número da conta', with: '123.555-7'
+      click_on 'Atualizar'
+    
+      expect(page).to have_content('Opção atualizada com sucesso')
+      expect(page).to have_content('001')
+      expect(page).to have_content('2150')
+      expect(page).to have_content('123.555-7')
     end
   end
 end
