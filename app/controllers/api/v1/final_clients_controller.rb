6d0a3ec70739170c1ec15aa09e4e1e650048c330
Mariana_C_Ruiz <mchierandaruiz0@gmail.com>
2021-06-30 06:11:10 -0300

    improving final client api

--- app/controllers/api/v1/final_clients_controller.rb
+++ app/controllers/api/v1/final_clients_controller.rb
@@ -1,18 +1,19 @@
 class Api::V1::FinalClientsController < ActionController::API
   
   def final_client_token
+    @same = false
     @final_client = FinalClient.new(final_client_params)
     @company = Company.find_by(token: params[:company_token])
     @final_client.save!
     @company.company_clients.create!(final_client: @final_client)
     render json: @final_client, status: 201
-  rescue ActiveRecord::RecordInvalid                       #ActionController::ParameterMissing
+  rescue ActiveRecord::RecordInvalid
     @client = FinalClient.where(cpf: @final_client.cpf).first
-    if @client && CompanyClient.where(company: @company, final_client: @client).empty? && !FinalClient.where(cpf: @client.cpf).empty?
-      @final_client = FinalClient.find_by(cpf: @client.cpf)
-      @company_client = @company.company_clients.create!(final_client: @client)
+    same_company?
+    different_company?
+    if @same == false
       render json: FinalClient.find_by(cpf: @client.cpf), status: 202
-    elsif @client && !CompanyClient.where(company: @company, final_client: @client).empty?
+    elsif @same == true
       render json: @final_client.errors, status: 409
     end
   rescue ActionController::ParameterMissing
@@ -24,5 +25,19 @@ class Api::V1::FinalClientsController < ActionController::API
   def final_client_params
     params.require(:final_client).permit(:name, :cpf)
   end
-  
+
+  def different_company?
+    if @client && CompanyClient.where(company: @company, final_client: @client).empty? && !FinalClient.where(cpf: @client.cpf).empty?
+      @final_client = FinalClient.find_by(cpf: @client.cpf)
+      @company_client = @company.company_clients.create!(final_client: @client)
+      @same = false
+    end
+  end
+
+  def same_company?
+    if @client && !CompanyClient.where(company: @company, final_client: @client).empty?
+      @same = true
+    end
+  end
+    
 end
