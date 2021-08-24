fd29ea8f3f2999749b41b8862ffc451263755fce
Mariana_C_Ruiz <mchierandaruiz0@gmail.com>
2021-08-24 18:55:10 -0300

    improved api final client

--- app/controllers/api/v1/final_clients_controller.rb
+++ app/controllers/api/v1/final_clients_controller.rb
@@ -6,28 +6,13 @@ class Api::V1::FinalClientsController < ActionController::API
     @company.company_clients.create!(final_client: @final_client)
     render json: @final_client, status: :created
   rescue ActiveRecord::RecordInvalid
-    same_company?
-    different_company?
-    if @same == false
-      render json: FinalClient.find_by(cpf: @client.cpf), status: :accepted
-    elsif @same == true
-      render json: @final_client.errors, status: :conflict
-    end
+    testing_company
   end
 
   private
 
   def final_client_params
     params.require(:final_client).permit(:name, :cpf)
-  end
-
-  def different_company?
-    if @client && CompanyClient.where(company: @company,
-                                      final_client: @client).empty? && !FinalClient.where(cpf: @client.cpf).empty?
-      @final_client = FinalClient.find_by(cpf: @client.cpf)
-      @company_client = @company.company_clients.create!(final_client: @client)
-      @same = false
-    end
   end
 
   def find_contents
@@ -39,7 +24,27 @@ class Api::V1::FinalClientsController < ActionController::API
     render status: :precondition_failed, json: { errors: 'parâmetros inválidos' }
   end
 
+  def testing_company
+    same_company?
+    different_company?
+    case @same
+    when false
+      render json: FinalClient.find_by(cpf: @client.cpf), status: :accepted
+    when true
+      render json: @final_client.errors, status: :conflict
+    end
+  end
+
   def same_company?
     @same = true if @client && !CompanyClient.where(company: @company, final_client: @client).empty?
+  end
+
+  def different_company?
+    if @client && CompanyClient.where(company: @company,
+                                      final_client: @client).empty? && !FinalClient.where(cpf: @client.cpf).empty?
+      @final_client = FinalClient.find_by(cpf: @client.cpf)
+      @company_client = @company.company_clients.create!(final_client: @client)
+      @same = false
+    end
   end
 end
