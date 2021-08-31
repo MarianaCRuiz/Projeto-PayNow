e7823c5d6750c6ef5da6cd31cf51f51fc3271769
Mariana_C_Ruiz <mchierandaruiz0@gmail.com>
2021-08-30 21:28:39 -0300

    validate email

--- app/validators/email_validator.rb
+++ app/validators/email_validator.rb
@@ -1,12 +1,20 @@
 require 'mail'
 class EmailValidator < ActiveModel::EachValidator
   def validate_each(record, _attribute, value)
-    m = Mail::Address.new(value)
-    r = m.domain.present? && m.domain.match('\.') && m.address == value
-    domain = 'gmail.com yahoo.com hotmail.com msn.com yahoo.com.br outlook.com uol.com.br terra.com.br'
-    domain = domain.split
-    r = false if domain.any?(m.domain) == true
-    r = false if m.domain == 'paynow.com.br' && Admin.where(email: m.address).empty?
-    record.errors.add(:attribute, 'email inválido') unless r
+    find_domain
+    validate_domain(value)
+    record.errors.add(:attribute, 'email inválido') unless @r
+  end
+
+  def find_domain
+    @domain = 'gmail.com yahoo.com hotmail.com msn.com yahoo.com.br outlook.com uol.com.br terra.com.br'
+    @domain = @domain.split
+  end
+
+  def validate_domain(value)
+    @m = Mail::Address.new(value)
+    @r = @m.domain.present? && @m.domain.match('\.') && @m.address == value
+    @r = false if @domain.any?(@m.domain) == true
+    @r = false if @m.domain == 'paynow.com.br' && Admin.where(email: @m.address).empty?
   end
 end
